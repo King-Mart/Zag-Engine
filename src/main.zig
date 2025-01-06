f04f9dde236b9307be52315a235f2cc3338a330d
King-Mart <ken.mart.d@gmail.com>
2025-01-05 20:12:05 -0500

    Managed to abstract making a window!

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,7 @@ const Ziglib = @import("Ziglib");
 const config = @import("config");
 pub const UNICODE = config.UNICODE;
 
+
 //What is this?
 // pub fn panic(msg: []const u8, error_return_trace: ?*std.builtin.StackTrace) noreturn {
 //     _ = error_return_trace;
@@ -10,7 +11,62 @@ pub const UNICODE = config.UNICODE;
 // }
 
 
-const test_window = Ziglib.window.window;
+
+
+
+pub fn wWinMain(hInstance: Ziglib.window.HINSTANCE, hPrevInstance: ?Ziglib.window.HINSTANCE, lpCmdLine: ?[*]u16, nCmdShow: i32) i32 {
+    std.debug.print("wWinMain called\n", .{});
+    // Mark unused parameters as unused
+    _ = nCmdShow;
+    _ = hPrevInstance;
+    const test_window = Ziglib.window.window;
+    // If lpCmdLine is not null, handle it properly
+    if (lpCmdLine) |cmdLine| {
+        var cmdLineLen: usize = 0;
+        while (cmdLine[cmdLineLen] != 0) {
+            cmdLineLen += 1;
+        }
+        const cmdLineSlice = cmdLine[0..cmdLineLen];
+        var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+        defer arena.deinit();
+        var utf8CmdLineBuf = arena.allocator().alloc(u8, cmdLineLen * 4) catch |err| {
+            std.debug.print("Error allocating memory: {s}\n", .{@errorName(err)});
+            return 1;
+        };
+        defer arena.allocator().free(utf8CmdLineBuf);
+        const utf8CmdLineLen = std.unicode.utf16leToUtf8(utf8CmdLineBuf, cmdLineSlice) catch |err| {
+            std.debug.print("Error converting UTF-16 to UTF-8: {s}\n", .{@errorName(err)});
+            return 1;
+        };
+        std.debug.print("Command line: {s}\n", .{utf8CmdLineBuf[0..utf8CmdLineLen]});
+    } else {
+        std.debug.print("No command line arguments\n", .{});
+    }
+    test_window.createWindowClass(hInstance) catch |err| {
+        std.debug.print("Error creating window class: {s}\n", .{@errorName(err)});
+        return 1;
+    };
+    _ = test_window.registerClass();
+
+    test_window.createWindow() catch |err| {
+        std.debug.print("Error creating window: {s}\n", .{@errorName(err)});
+        return 1;
+    };
+    test_window.showWindow() catch |err| {
+        std.debug.print("Error showing window: {s}\n", .{@errorName(err)});
+        return 1;
+    };
+    test_window.updateWindow() catch |err| {
+        std.debug.print("Error updating window: {s}\n", .{@errorName(err)});
+        return 1;
+    };
+    test_window.messageLoop() catch |err| {
+        std.debug.print("Error in message loop: {s}\n", .{@errorName(err)});
+        return 1;
+    };
+    return 0;
+    
+}
 pub fn main() !void {
     if (config.UNICODE == false) {
         @compileError("Non Unicode is not supported");
@@ -27,7 +83,7 @@ pub fn main() !void {
     //there are no tests yet
     // const stdout = bw.writer();
     //TODO: add tests and handle errors properly
-    try test_window.init();
+
     // try stdout.print("Run `zig build test` to run the tests.\n", .{});
 
     try bw.flush(); // don't forget to flush!
