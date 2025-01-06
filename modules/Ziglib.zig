31c16b73265f574a07053c9035455ee59e88771d
King-Mart <ken.mart.d@gmail.com>
2025-01-06 16:41:46 -0500

    Added the networking module file

--- modules/Ziglib.zig
+++ modules/Ziglib.zig
@@ -4,6 +4,8 @@ pub const input = @import("inputHandler/input.zig");
 pub const graphics = @import("graphicRenderer/graphics.zig");
 pub const math = @import("math/math.zig");
 pub const file = @import("file/file.zig");
+pub const core = @import("core.zig");
+pub const network = @import("networking/network.zig");
 
 test {
     @import("std").testing.refAllDecls(@This());
