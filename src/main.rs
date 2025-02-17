f21889bff029da675035c31f1a223b8b61a017e1
King-Mart <ken.mart.d@gmail.com>
2025-02-17 14:28:50 -0500

    Its Coming together

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,50 @@
-use bindings::windows::Win32::Foundation;
 
+//use bindings::windows::Win32::Foundation;
+use bindings::windows::Win32::UI::WindowsAndMessaging;
+// use bindings::windows::Win32::UI::Input::KeyboardAndMouse;
 
+use bindings::windows::Win32::Foundation::{
+    HWND, 
+    HINSTANCE,
+    WPARAM,
+    LPARAM,
+    LRESULT,
+};
+use std::os::windows::ffi::OsStrExt;
 
+//PLACEHOLDER< TODO: UNDERSTAND and MODIFY this
+fn to_wide_string(value: &str) -> Vec<u16> {
+    std::ffi::OsStr::new(value).encode_wide().chain(std::iter::once(0)).collect()
+}
+extern "system" fn wnd_proc(hwnd: HWND, msg: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+    unsafe { WindowsAndMessaging::DefWindowProcW(hwnd, msg, wparam, lparam) }
+}
 
 fn main() {
+    let hmodule = unsafe {bindings::windows::Win32::System::LibraryLoader::GetModuleHandleW(None)};
+    //TODO: Add a unsuccesfull initialization handling
+    let instance = HINSTANCE(hmodule.unwrap().0);
+
+    let class_name = to_wide_string("sample_class");
+    let class_name_pcwstr = bindings::windows::core::PCWSTR(class_name.as_ptr());
+
+    
+    let _wc = WindowsAndMessaging::WNDCLASSEXW {
+        style: WindowsAndMessaging::CS_CLASSDC,
+        //I need to understand this Rust lyrical magical
+        lpfnWndProc: Some(wnd_proc),
+        cbClsExtra: 0,
+        cbSize: std::mem::size_of::<WindowsAndMessaging::WNDCLASSEXW>() as u32,
+        cbWndExtra: 0,
+        hInstance: instance,
+        //more Rust lyrical miracle
+        hIcon: Default::default(),
+        hIconSm: Default::default(),
+        hCursor: Default::default(),
+        hbrBackground: Default::default(),
+        lpszMenuName: bindings::windows::core::PCWSTR::null(),
+        lpszClassName: class_name_pcwstr,
+    };
     println!("Hello, world!");
     println!("Printing something");
     let variable = 3;
--- zaglib/src/window.rs
+++ zaglib/src/window.rs
@@ -1,3 +1,3 @@
 //Will represent a standalone window class. What I would like to implement next is separation from the window API in order to simplify cross platform development.
 
-use bindings::windows::Win32::Foundation::HWND;
+// use bindings::windows::Win32::Foundation::HWND;
