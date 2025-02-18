03d25a35e619f5abcf3fde02c99f4c48581d725c
King-Mart <ken.mart.d@gmail.com>
2025-02-18 18:45:47 -0500

    A window with two issues

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
 
+use bindings::windows::core::PCWSTR;
 //use bindings::windows::Win32::Foundation;
-use bindings::windows::Win32::UI::WindowsAndMessaging;
+use bindings::windows::Win32::UI::WindowsAndMessaging::*;
 // use bindings::windows::Win32::UI::Input::KeyboardAndMouse;
 
 use bindings::windows::Win32::Foundation::{
-    HWND, 
-    HINSTANCE,
-    WPARAM,
-    LPARAM,
-    LRESULT,
+    HINSTANCE, HWND, LPARAM, LRESULT, WPARAM
 };
 use std::os::windows::ffi::OsStrExt;
 
@@ -16,37 +13,105 @@ use std::os::windows::ffi::OsStrExt;
 fn to_wide_string(value: &str) -> Vec<u16> {
     std::ffi::OsStr::new(value).encode_wide().chain(std::iter::once(0)).collect()
 }
+fn to_pcwstr(value: &str) -> PCWSTR {
+    return PCWSTR(to_wide_string(value).as_ptr())
+}
 extern "system" fn wnd_proc(hwnd: HWND, msg: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
-    unsafe { WindowsAndMessaging::DefWindowProcW(hwnd, msg, wparam, lparam) }
+    match msg {
+        WM_CLOSE => {
+            _ = unsafe { DestroyWindow(hwnd) };
+            return LRESULT(0);
+        }
+        WM_DESTROY => {
+            unsafe { PostQuitMessage(0) };
+            return LRESULT(0);
+        }
+        _ => unsafe { DefWindowProcW(hwnd, msg, wparam, lparam) }
+    }
 }
 
-fn main() {
-    let hmodule = unsafe {bindings::windows::Win32::System::LibraryLoader::GetModuleHandleW(None)};
-    //TODO: Add a unsuccesfull initialization handling
-    let instance = HINSTANCE(hmodule.unwrap().0);
 
-    let class_name = to_wide_string("sample_class");
-    let class_name_pcwstr = bindings::windows::core::PCWSTR(class_name.as_ptr());
+fn main() {
+   //TODO: Add a unsuccesfull initialization handling
+    let instance = match unsafe { bindings::windows::Win32::System::LibraryLoader::GetModuleHandleW(None) } {
+        Ok(hmodule) => HINSTANCE(hmodule.0),
+        Err(e) => {
+            eprintln!("Failed to get HINSTANCE (init1): {:?}", e);
+            return;
+        }
+    };
+    
+
+    let class_name = to_pcwstr("sample_class");
 
     
-    let _wc = WindowsAndMessaging::WNDCLASSEXW {
-        style: WindowsAndMessaging::CS_CLASSDC,
+    let wc = WNDCLASSEXW {
+        style: CS_CLASSDC,
         //I need to understand this Rust lyrical miracle
         lpfnWndProc: Some(wnd_proc),
         cbClsExtra: 0,
-        cbSize: std::mem::size_of::<WindowsAndMessaging::WNDCLASSEXW>() as u32,
+        cbSize: std::mem::size_of::<WNDCLASSEXW>() as u32,
         cbWndExtra: 0,
         hInstance: instance,
-        //more Rust lyrical miracle
+        //Since we need to gives values at the start and we do not know them yet, we pass on the defaults
         hIcon: Default::default(),
         hIconSm: Default::default(),
         hCursor: Default::default(),
         hbrBackground: Default::default(),
-        lpszMenuName: bindings::windows::core::PCWSTR::null(),
-        lpszClassName: class_name_pcwstr,
+        lpszMenuName: PCWSTR::null(),
+        lpszClassName: class_name,
     };
-    println!("Hello, world!");
-    println!("Printing something");
-    let variable = 3;
-    println!("The value of the variable I just made is {}", variable);
+     if unsafe { 
+            RegisterClassExW(&wc) == 0
+        } {
+            eprintln!("Failed to register the window class (init2")
+        }
+
+    let hwnd = 
+        match          
+            unsafe {
+                    CreateWindowExW(
+                    WS_EX_OVERLAPPEDWINDOW,
+                    class_name,
+                    to_pcwstr("Test"),
+                    WS_OVERLAPPEDWINDOW,
+                    CW_USEDEFAULT,
+                    CW_USEDEFAULT,
+                    900,
+                    1600,
+                    None,
+                    None,
+                    Some(instance),
+                    None
+                )
+            } {
+                Ok(hw) => hw,
+                Err(error) => {
+                    eprintln!("Failed to create the window (init3): {:?}",error);
+                    panic!("Aborting")
+                }
+
+            };
+    unsafe {
+        _ = ShowWindow(hwnd, SW_SHOW);
+        _ = bindings::windows::Win32::Graphics::Gdi::UpdateWindow(hwnd);
+    }
+    let mut msg: MSG = unsafe { std::mem::zeroed() };
+    loop {
+        let ret = unsafe { GetMessageW(&mut msg, None, 0, 0) };
+    
+        if ret.0 == -1 {
+            eprintln!("GetMessageW failed!");
+            break;
+        } else if ret.0 == 0 {
+            // WM_QUIT received
+            break;
+        }
+    
+        unsafe {
+            _ = TranslateMessage(&msg);
+            DispatchMessageW(&msg);
+        }
+    }
 }
+    
